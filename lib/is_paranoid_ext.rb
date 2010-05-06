a5caea5791932febb453cc245750f7c7aef9e001
Alex Neill <alex.neill@gmail.com>
2010-05-06 15:02:28 +0100

    Adding support for :through

--- lib/is_paranoid_ext.rb
+++ lib/is_paranoid_ext.rb
@@ -15,9 +15,28 @@ module IsParanoidExt
   private
   
   def merge_paranoid_options!(association_id, options)
-    if (klass = association_id.to_s.classify.constantize) && klass.respond_to?(:restore)
-      options.merge!(:conditions => ["#{klass.quoted_table_name}.#{klass.destroyed_field} IS ?", klass.field_not_destroyed])
+    through_id = options[:through]
+    @paranoid_conditions, @paranoid_values = nil, nil
+    apply_paranoid_conditions_for(options[:class_name] || association_id)
+    apply_paranoid_conditions_for through_id if through_id
+    options.merge!(:conditions => merged_paranoid_conditions) unless @paranoid_conditions.nil?
+  end
+  
+  def apply_paranoid_conditions_for(association_id)
+    reflection = reflections[association_id.to_sym]
+    association_klass = ( reflection.try(:klass) || association_id.to_s.classify.constantize )
+    if association_klass && association_klass.respond_to?(:restore)
+      @paranoid_conditions ||= []
+      @paranoid_conditions << "#{association_klass.quoted_table_name}.#{association_klass.destroyed_field} IS ?"
+      @paranoid_values ||= []
+      @paranoid_values << association_klass.field_not_destroyed
     end
+  end
+  
+  def merged_paranoid_conditions
+    @merged_paranoid_conditions ||= if @paranoid_conditions.any? && @paranoid_values.any?
+      [@paranoid_conditions.map{|c|"(#{c})"}.join(' AND ')] + @paranoid_values
+    else nil end
   end
 end
 
