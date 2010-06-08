c045caf1728299af86d59e6092c1aaae7180a9a5
Alex Neill <alex.neill@gmail.com>
2010-06-08 11:25:46 +0100

    Added support for :include

--- lib/is_paranoid_ext.rb
+++ lib/is_paranoid_ext.rb
@@ -2,31 +2,40 @@ require 'activerecord'
 
 module IsParanoidExt
   
-  def preload_has_one_association(records, reflection, preload_options = {})
-    super(records, reflection, paranoid_preload_options(reflection, preload_options))
-  end
-  
-  def preload_has_many_association(records, reflection, preload_options = {})
-    super(records, reflection, paranoid_preload_options(reflection, preload_options))
-  end
-  
-  def preload_has_and_belongs_to_many_association(records, reflection, preload_options = {})
-    super(records, reflection, paranoid_preload_options(reflection, preload_options))
-  end
-
   private
 
-  def paranoid_preload_options(reflection, preload_options)
-    paranoid_preload_opts = preload_options.dup
-    if reflection.klass.respond_to?(:restore)
-      sql_array  = ["#{reflection.klass.quoted_table_name}.#{reflection.klass.destroyed_field} IS ?"]
-      sql_array << reflection.klass.field_not_destroyed
-      preload_conditions = paranoid_preload_opts.delete(:conditions) || []
-      paranoid_preload_opts.merge(:conditions => merge_conditions(sql_array, preload_conditions))
+    def construct_finder_sql_with_included_associations(options, join_dependency)
+      preload_options = options.dup
+      join_dependency.reflections.each do |reflection|
+        preload_options = paranoid_preload_options(reflection, preload_options)
+      end
+      super(preload_options, join_dependency)
     end
-    paranoid_preload_opts
-  end
+    
+    def preload_has_one_association(records, reflection, preload_options = {})
+      super(records, reflection, paranoid_preload_options(reflection, preload_options))
+    end
+    
+    def preload_has_many_association(records, reflection, preload_options = {})
+      super(records, reflection, paranoid_preload_options(reflection, preload_options))
+    end
+    
+    def preload_has_and_belongs_to_many_association(records, reflection, preload_options = {})
+      super(records, reflection, paranoid_preload_options(reflection, preload_options))
+    end
+    
+    def paranoid_preload_options(reflection, preload_options)
+      paranoid_preload_opts = preload_options.dup
+      if reflection.klass.respond_to?(:restore)
+        sql_array  = ["#{reflection.klass.quoted_table_name}.#{reflection.klass.destroyed_field} IS ?"]
+        sql_array << reflection.klass.field_not_destroyed
+        preload_conditions = paranoid_preload_opts.delete(:conditions) || []
+        paranoid_preload_opts.merge!(:conditions => merge_conditions(sql_array, preload_conditions))
+      end
+      paranoid_preload_opts
+    end
 
 end
 
 ActiveRecord::Base.send(:extend, IsParanoidExt)
+
